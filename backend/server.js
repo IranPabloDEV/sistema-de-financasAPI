c1701ce46650c4c31234e5c1b0231a901e11226b
Iran Pablo <martins.iran018@gmail.com>
2026-06-06 04:18:18 -0300

    correcao de erros endpoints e localhost

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.get("/", (req, res) => {
   res.send("API Controle Financeiro funcionando!");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
